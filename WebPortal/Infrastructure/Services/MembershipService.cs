b699e1477802055cebeda6219bb188fbbe2fa0d1
ashan <madu2000official@gmai.com>
2018-02-25 21:38:50 +0530

    adding service for membership/role managing for website

---
+++ WebPortal/Infrastructure/Services/MembershipService.cs
@@ -0,0 +1,32 @@
+﻿using PhotographyEye.Infrastructure.Repositories.Abstract;
+using PhotographyEye.Infrastructure.Services.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhotographyEye.Infrastructure.Services
+{
+    public class MembershipService // : IMembershipService
+    {
+        #region fields
+        private readonly IUserRepository _userRepository;
+        private readonly IRoleRepository _roleRepository;
+        private readonly IUserRoleRepository _userRoleRepository;
+        private readonly IEncryptionService _encryptionService;
+        #endregion
+
+        public MembershipService(
+            IUserRepository userRepository,
+            IRoleRepository roleRepository,
+            IUserRoleRepository userRoleRepository,
+            IEncryptionService encryptionService)
+        {
+            _userRepository = userRepository;
+            _roleRepository = roleRepository;
+            _userRoleRepository = userRoleRepository;
+            _encryptionService = encryptionService;
+        }
+
+    }
+}
